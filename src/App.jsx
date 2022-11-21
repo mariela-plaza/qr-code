376420a59034ac27815c75b0526d6c12876622d1
Mariela Plaza <mariela.plaza92@gmail.com>
2022-11-21 19:43:46 +0100

    Add basic layout and styles to QR card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import qrLogo from "./assets/images/image-qr-code.png";
 
 function App() {
   return (
     <div className="card">
-      <p>QR Logo</p>
+      <div>
+        <img src={qrLogo} alt="qr code" />
+      </div>
+      <h1>Improve your frontend skills by building projects</h1>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
     </div>
   );
 }
